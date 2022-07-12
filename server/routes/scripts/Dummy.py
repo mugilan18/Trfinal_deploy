eac282ce1e6e6503fc383f63fcb196909fa27a59
mugilan18 <mugilan.learny@gmail.com>
2022-07-13 00:38:44 +0530

    latest deploy

--- server/routes/scripts/Dummy.py
+++ server/routes/scripts/Dummy.py
@@ -3,12 +3,13 @@ class Dummy:
   def __init__(self, arg):
         self.arg = arg
   def dummy(self):
-        argument = self.arg
-        one = argument[0].split( )
-        start=len(one)
-        two = argument[1].split( )
-        length=len(two)
-        end=start-length
-        endresult = one[1 :end]
-        endresult =  " ".join(endresult)
-        print(json.dumps({"result":[{"output" : str(endresult) }]}))
+      #   argument = self.arg
+      #   one = argument[0].split( )
+      #   start=len(one)
+      #   two = argument[1].split( )
+      #   length=len(two)
+      #   end=start-length
+      #   endresult = one[1 :end]
+      #   endresult =  " ".join(endresult)
+      #   print(json.dumps({"result":[{"output" : str(endresult) }]}))
+        print(json.dumps({"result":[{"output" : str("No calculations Direct result") }]}))
